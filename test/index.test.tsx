a86605be72a83647c3668ed7a88e0cf1f27f63d2
Miriam Zusin <miriamzusin@gmail.com>
2023-04-19 23:31:58 +0300

    unit tests

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -4,6 +4,7 @@ import { cleanup, render, fireEvent } from '@testing-library/react'; // https://
 afterEach(cleanup);
 
 describe('Input Number', () => {
+
     test('Initial value below the provided minimum', () => {
 
         const { container} = render(
@@ -14,13 +15,61 @@ describe('Input Number', () => {
                 value={ 2.2 }
             />
         );
+        const $input = container.querySelector('input') as HTMLInputElement;
 
+        expect($input.value).toStrictEqual('10');
+    });
+
+    test('Initial value above the provided maximum', () => {
+
+        const { container} = render(
+            <InputNumber
+                min={ 10 }
+                max={ 100 }
+                step={ 0.01 }
+                value={ 101 }
+            />
+        );
+        const $input = container.querySelector('input') as HTMLInputElement;
+
+        expect($input.value).toStrictEqual('100');
+    });
+
+    test('ArrowDown', () => {
+
+        const { container} = render(
+            <InputNumber
+                min={ 10 }
+                max={ 100 }
+                step={ 1 }
+                value={ 100 }
+            />
+        );
         const $input = container.querySelector('input') as HTMLInputElement;
 
         fireEvent.keyDown($input, {
             key: 'ArrowDown',
         });
 
-        expect($input.value).toStrictEqual('10');
+        expect($input.value).toStrictEqual('99');
+    });
+
+    test('ArrowUp', () => {
+
+        const { container} = render(
+            <InputNumber
+                min={ 10 }
+                max={ 100 }
+                step={ 1 }
+                value={ 10 }
+            />
+        );
+        const $input = container.querySelector('input') as HTMLInputElement;
+
+        fireEvent.keyDown($input, {
+            key: 'ArrowUp',
+        });
+
+        expect($input.value).toStrictEqual('11');
     });
 });
