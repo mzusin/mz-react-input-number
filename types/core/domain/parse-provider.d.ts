cdffda504a830db723c48b3773f0443c3a899429
Miriam Zusin <miriamzusin@gmail.com>
2023-04-25 04:46:00 +0300

    remove types folder

--- types/core/domain/parse-provider.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Return number or NaN.
- */
-export declare const parseNumber: (text: string | number | undefined) => number;
--- types/core/ui/InputNumber.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { IInputNumber } from '../interfaces';
-export declare const DEFAULT_DECIMAL_PLACES = 4;
-export declare const InputNumber: (props: IInputNumber) => JSX.Element;
